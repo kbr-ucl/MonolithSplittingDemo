e7bd8fe17b50c86c5db8465edbbc81accef40a60
Kaj Steen Bromose <KBR8776@ucl.dk>
2020-04-01 22:36:37 +0200

    Folder restructoring. MVC project added

---
+++ src/3.1/Mvc/MvcMovie/Models/MovieGenreViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MvcMovie.Models
+{
+    public class MovieGenreViewModel
+    {
+        public SelectList Genres;
+        public List<Movie> Movies;
+        public string MovieGenre { get; set; }
+        public string SearchString { get; set; }
+    }
+}
