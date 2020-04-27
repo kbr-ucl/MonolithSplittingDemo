b9b14b25cb762ad8da147b1fda223abdab33bf15
Kaj Steen Bromose <KBR8776@ucl.dk>
2020-04-27 11:51:08 +0200

    RowVersion added to MovieViewModel

--- src/3.1/WithApi/MvcMovie.Ui.Mvc/Models/MovieViewModel.cs
+++ src/3.1/WithApi/MvcMovie.Ui.Mvc/Models/MovieViewModel.cs
@@ -23,5 +23,7 @@ namespace MvcMovie.Ui.Mvc.Models
 
         [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(5)]
         public string Rating { get; set; }
+
+        public byte[] RowVersion { get; set; }
     }
 }
