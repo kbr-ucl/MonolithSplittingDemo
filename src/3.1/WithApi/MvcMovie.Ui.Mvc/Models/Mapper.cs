06b29e731edee6b041bfab5909d7092cf0bdc5bf
Kaj Steen Bromose <KBR8776@ucl.dk>
2020-04-27 12:18:55 +0200

    RowVersion added to Mapper.cs

--- src/3.1/WithApi/MvcMovie.Ui.Mvc/Models/Mapper.cs
+++ src/3.1/WithApi/MvcMovie.Ui.Mvc/Models/Mapper.cs
@@ -9,7 +9,7 @@ namespace MvcMovie.Ui.Mvc.Models
         public static MovieViewModel Map(MovieDto dto)
         {
             return new MovieViewModel
-                {Genre = dto.Genre, Id = dto.Id, Price = dto.Price, ReleaseDate = dto.ReleaseDate, Title = dto.Title};
+                {Genre = dto.Genre, Id = dto.Id, Price = dto.Price, ReleaseDate = dto.ReleaseDate, Title = dto.Title, RowVersion = dto.RowVersion};
         }
 
         public static IEnumerable<MovieViewModel> Map(IEnumerable<MovieDto> dtos)
@@ -20,7 +20,7 @@ namespace MvcMovie.Ui.Mvc.Models
         public static MovieDto Map(MovieViewModel view)
         {
             return new MovieDto
-                {Genre = view.Genre, Id = view.Id, Price = view.Price, ReleaseDate = view.ReleaseDate, Title = view.Title};
+                {Genre = view.Genre, Id = view.Id, Price = view.Price, ReleaseDate = view.ReleaseDate, Title = view.Title, RowVersion = view.RowVersion};
         }
     }
 }
